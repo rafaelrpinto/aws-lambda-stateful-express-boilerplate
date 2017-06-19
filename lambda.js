125dd943a4e364ba89f790656261e224f03f5f99
Rafael P <raprp@posteo.de>
2017-06-19 07:25:04 +0100

    Initial commit

---
+++ lambda.js
@@ -0,0 +1,6 @@
+const awsServerlessExpress = require('aws-serverless-express');
+// Import the transpiled version
+const app = require('./dist/app');
+
+const server = awsServerlessExpress.createServer(app);
+exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
