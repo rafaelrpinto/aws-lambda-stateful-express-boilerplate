54ec6130e2bc47209e9fb3bbf5a017d37d711381
Rafael Pinto <raprp@posteo.de>
2017-07-22 08:51:04 +0100

    Version upgrade

--- lambda.js
+++ lambda.js
@@ -1,6 +1,7 @@
 // This is the lambda entry point
 const awsServerlessExpress = require('aws-serverless-express');
 // Import the transpiled version of app
+// eslint-disable-next-line
 const app = require('./.dist/server/app');
 
 const server = awsServerlessExpress.createServer(app);
